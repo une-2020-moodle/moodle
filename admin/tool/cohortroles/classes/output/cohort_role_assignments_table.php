d45ab0862eddcf0d65108d53b889bd4dbb69d3c4
Brendan Heywood <brendan@catalyst-au.net>
2020-06-05 13:40:25 +1000

    MDL-61021 tool_cohortrole: Convert to action table
    Co-authored-by: Matthew Tolmie <tolmie.matthew@gmail.com>
    Co-authored-by: Kerrod Young <kerrodyoung@gmail.com>

--- admin/tool/cohortroles/classes/output/cohort_role_assignments_table.php
+++ admin/tool/cohortroles/classes/output/cohort_role_assignments_table.php
@@ -41,6 +41,7 @@ use table_sql;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class cohort_role_assignments_table extends table_sql {
+    use \action_table_trait;
 
     /**
      * Sets up the table.
@@ -107,19 +108,38 @@ class cohort_role_assignments_table extends table_sql {
     }
 
     /**
-     * Actions column.
+     * Used by the action_table_trait (col_actions function) to
+     * render the table's actions.
+     * 
+     * Uses an action_menu compiled of action_links,
+     * i.e. action_link(url, text, component_action, attributes, icon)
      *
-     * @param array $data Row data.
-     * @return string
+     * @param  object $row
+     * @return action_menu $action_group The actions for the table.
      */
-    protected function col_actions($data) {
-        global $OUTPUT;
+    public function get_table_actions($row) {
+        $deleteurl = new moodle_url($this->baseurl);
+        $deleteurl->params(['removecohortroleassignment' => $data->id,
+            'sesskey' => sesskey()]
+        );
 
-        $action = new \confirm_action(get_string('removecohortroleassignmentconfirm', 'tool_cohortroles'));
-        $url = new moodle_url($this->baseurl);
-        $url->params(array('removecohortroleassignment' => $data->id, 'sesskey' => sesskey()));
-        $pix = new \pix_icon('t/delete', get_string('removecohortroleassignment', 'tool_cohortroles'));
-        return $OUTPUT->action_link($url, '', $action, null, $pix);
+        $action_group->add_secondary_action(
+            new \action_link(
+                $deleteurl,
+                get_string('removecohortroleassignment'),
+                new \confirm_action(get_string(
+                    'removecohortroleassignmentconfirm',
+                    'tool_cohortroles')
+                ),
+                null,
+                new \pix_icon(
+                    't/delete',
+                    get_string('removecohortroleassignment', 'tool_cohortroles')
+                )
+            )
+        );
+        
+        return $action_group;
     }
 
     /**
